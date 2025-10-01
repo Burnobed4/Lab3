5dc207d4aa6ae6ba77141d8ca701bf49aecb4f28
Artem Litvinenko <burnobed44@gmail.com>
2025-10-01 14:43:58 +0300

    2 zadanie

--- lab3.py
+++ lab3.py
@@ -1,3 +1,7 @@
 #Задание 1
 squares = [x**2 for x in range(1, 11)]
 print("1. Квадраты чисел:", squares)
+
+#Задание 2
+even_numbers = [x for x in range(1, 20) if x % 2 == 0]
+print("2. Чётные числа:", even_numbers)
