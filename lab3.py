aee5de7c528931a4003707fdb802ae3a72bef823
Artem Litvinenko <burnobed44@gmail.com>
2025-10-01 14:46:27 +0300

    3 zadanie

--- lab3.py
+++ lab3.py
@@ -5,3 +5,8 @@ print("1. Квадраты чисел:", squares)
 #Задание 2
 even_numbers = [x for x in range(1, 20) if x % 2 == 0]
 print("2. Чётные числа:", even_numbers)
+
+#Задание 3
+words = ["python", "Java", "c++", "Rust", "go"]
+filtered_words = [word.upper() for word in words if len(word) > 3]
+print("3. Отфильтрованные слова:", filtered_words)
